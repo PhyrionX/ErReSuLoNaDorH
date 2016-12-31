9b426aa509597625f9f6d5331d9631ae783e4ed6
Daniel Uroz <uzetta27@hotmail.com>
2016-12-31 18:03:20 +0100

    File support

--- ErReSuLoNaDorH.py
+++ ErReSuLoNaDorH.py
@@ -6,14 +6,13 @@ from version import *
 def do_main():
     args = parse()
     check_args(args)
-    shurtext = do_magic(args.t)
-    print(shurtext)
 
 def parse():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-v", action="store_true", help="Display ErReSuLoNaDorH's version")
     parser.add_argument("-t", type=str, metavar="<text>", help="Text to ReSuLoNaRH")
+    parser.add_argument("-f", type=str, metavar="<path>", help="File to ReSuLoNaRH")
 
     return parser.parse_args(sys.argv[1:])
     
@@ -21,9 +20,14 @@ def check_args(args):
     if args.v:
         print_version()
         sys.exit(0)
-    if not args.t:
+    if args.f:
+        do_file_magic(args.f)
+    elif not args.t:
         print("[Error] no text specified (--text or --help)")
         sys.exit(-1)
+    else:
+        print(do_magic(args.t))
+
 
 def print_version():
     print("Version: %s" % ERESULONADORH_VERSION)
@@ -33,11 +37,15 @@ def do_magic(text):
     shurtext = ""
     for i, ch in enumerate(text):
         if (ch == ' '):
-            is_h_upper = random.choice([True, False])
-            if is_h_upper:
-                shurtext += 'H '
+            final_h = random.choice([True, False])
+            if final_h:
+                is_h_upper = random.choice([True, False])
+                if is_h_upper:
+                    shurtext += 'H '
+                else:
+                    shurtext += 'h '
             else:
-                shurtext += 'h '
+                shurtext += ' '
         else:
             if ((i != 0) and (i % 2 == 0)):
                 if ch.islower():
@@ -46,6 +54,12 @@ def do_magic(text):
                     shurtext += ch.lower()
             else:
                 shurtext += ch
-    return shurtext + "h"
+    if shurtext:
+        shurtext + "h"
+    return shurtext
 
+def do_file_magic(path):
+    with open(path) as f:
+        for line in f:
+            print(do_magic(line))
 do_main()
